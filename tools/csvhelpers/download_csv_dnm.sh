ddee59cb1ba0e3126d95e926cc6c5142dd1988cd
angie <angheloalf95@gmail.com>
2022-04-15 12:36:05 -0400

    dnm: Add gamestate_functions to csv download script

--- tools/csvhelpers/download_csv_dnm.sh
+++ tools/csvhelpers/download_csv_dnm.sh
@@ -17,10 +17,13 @@ wget "https://docs.google.com/spreadsheets/d/17WZXsAATDgXXSBBVEOc_zT-QJDkD72vaGj
 ## Functions
 wget "https://docs.google.com/spreadsheets/d/17WZXsAATDgXXSBBVEOc_zT-QJDkD72vaGjZ5YyG1a_Y/export?format=csv&gid=467726846"  -O - | tr -d '\r' > "dnm/tables/boot_functions.csv"
 wget "https://docs.google.com/spreadsheets/d/17WZXsAATDgXXSBBVEOc_zT-QJDkD72vaGjZ5YyG1a_Y/export?format=csv&gid=2064898020" -O - | tr -d '\r' > "dnm/tables/code_functions.csv"
+wget "https://docs.google.com/spreadsheets/d/17WZXsAATDgXXSBBVEOc_zT-QJDkD72vaGjZ5YyG1a_Y/export?format=csv&gid=105420436"  -O - | tr -d '\r' > "dnm/tables/gamestate_functions.csv"
 # Mix the different functions csvs into one
 cat "dnm/tables/boot_functions.csv" > "dnm/tables/functions.csv"
 echo >> "dnm/tables/functions.csv" # newline
 tail -n +3 "dnm/tables/code_functions.csv" >> "dnm/tables/functions.csv"
+echo >> "dnm/tables/functions.csv" # newline
+tail -n +3 "dnm/tables/gamestate_functions.csv" >> "dnm/tables/functions.csv"
 
 ## makerom
 wget "https://docs.google.com/spreadsheets/d/17WZXsAATDgXXSBBVEOc_zT-QJDkD72vaGjZ5YyG1a_Y/export?format=csv&gid=1355718882" -O - | tr -d '\r' > "dnm/tables/makerom.text.csv"
